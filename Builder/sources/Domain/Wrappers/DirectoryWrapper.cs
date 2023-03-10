90727a4d427befcacaa2af17f5c01d73a9199ccb
fernandoprass <fprass@gmail.com>
2023-03-10 19:00:53 +0000

    Start add unit tests to Data service

---
+++ Builder/sources/Domain/Wrappers/DirectoryWrapper.cs
@@ -0,0 +1,15 @@
+﻿namespace Builder.Domain.Wrappers
+{
+   public interface IDirectoryWrapper
+   {
+      string[] GetFiles(string path, string seacrhPattern, SearchOption searchOption);
+   }
+
+   public class DirectoryWrapper : IDirectoryWrapper
+   {
+      public string[] GetFiles(string path, string seacrhPattern, SearchOption searchOption)
+      {
+         return Directory.GetFiles(path, seacrhPattern, searchOption);
+      }  
+   }
+}
